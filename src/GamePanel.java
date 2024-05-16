acb56384279ceb7a646af3141f6db52b1f754ce1
MichielTr <r0832449@student.thomasmore.be>
2024-05-16 12:26:21 +0200

    added grid lines to game panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -41,10 +41,14 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g) {
-
+        for (int i = 0; i < screenHeight/unitSize; i++) {
+            g.drawLine(i*unitSize,0,i*unitSize,screenHeight);
+            g.drawLine(0,i*unitSize,screenWidth,i*unitSize);
+        }
     }
 
     public void newApple() {
